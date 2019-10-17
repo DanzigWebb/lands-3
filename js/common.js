7ee15a206c526cf260eb3a7faab3d53a190877ca
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-10-17 19:52:49 +0300

    c

---
+++ js/common.js
@@ -0,0 +1,12 @@
+var swiper = new Swiper('.swiper-container', {
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'progressbar',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  spaceBetween: 40
+});
+console.log('made by https://github.com/DanzigWebb')
